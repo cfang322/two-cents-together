4b3c7ec2d25bd0a2deb103f7d7665aca15ca1cc0
Yaqi Fang <cfang322@gmail.com>
2025-08-15 13:28:00 -0400

    Add detailed inline comments to authRoutes.js for cclarity and developer reference

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
+// This file is exposing your endpoints. It connects URLs to logic.
+
+// You create a router and pull in the logive from the controller.
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/authController');
 
+// If someone POSTs to /auth/signup, run the signup() function; same logic for login
 router.post('/signup', signup);
 router.post('/login', login);
 
+// export the router so it can be added to the main server.
 module.exports = router;
